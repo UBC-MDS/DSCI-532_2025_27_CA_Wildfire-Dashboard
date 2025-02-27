bf1fa926b39ac1326bcbcc1bfef825f5e47c69e6
Elaine Chu <chuelaine23@gmail.com>
2025-02-26 16:30:50 -0800

    Add input widget and create layout

--- src/app.py
+++ src/app.py
@@ -5,82 +5,58 @@ import dash_vega_components as dvc
 import pandas as pd
 import plotly.express as px
 from vega_datasets import data
-
+from data_import import load_calfire_df
 
 # Initiatlize the app
 app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 
-df = pd.read_csv("data/raw/California Wildfire Damage.csv", parse_dates=["Date"])
+# Load wildfire data
+calfire_df = load_calfire_df()
+
+# Components
+title = [html.H1('California Wildfire Dashboard'),
+         html.H6('A one-stop shop for gleaning insights from California Wildfires data')]
+global_widgets = [
+    dbc.Label("County"),
+    dcc.Dropdown(id='county', options=['A', 'B', 'C'], value = 'county'),
+    dbc.Label("Incident ID"),
+    dcc.Dropdown(id="Incident ID", options=['A', 'B', 'C'], value = 'id'),
+    dbc.Label("Year"),
+    dcc.RangeSlider(id='year',
+                    min=1990,
+                    max=2025,
+                    value=[1990, 2025],
+                    marks={year: str(year) for year in range(1950, 2026, 5)},
+                    updatemode='drag')
+] # inputs
+cali_map = [html.H3('California map')] # map of california with
+sum_cost=[html.H3('summary cost')]# total lost value 
+damage_level=[html.H3('damage level')]# pie chart of count of damage level
+time_cost=[html.H3('time series of cost')]# time series of cost of incidents
+structure_count=[html.H3('bar chart structure count')]# bar chart of damage by stucture category and county
+house_damage =[html.H3('house characteristic vs damage')]# house characteristic vs Damage level
 
 
 # Layout
 app.layout = dbc.Container([
-
-
-## Title
-    dcc.Markdown('''
-
-    # California Wildfires Dashboard
-
-    A one-stop shop for gleaning insights from California Wildfires data
-
-'''),
-
-html.Br(),
-
-
-## Dropdown / filter
-dcc.Markdown('''
-Select a location to see its associated scatter plot:
-'''),
-dcc.Dropdown(
-    id='location',
-    options=[{'label': loc, 'value': loc} for loc in df['Location'].unique()],
-    placeholder = "Select a location"
-),
-
-html.Br(),
-
-
-## Chart
-    dvc.Vega(
-        id='scatter',
-        spec={},
-        ),
-
-html.Br(),
-
-
-## TODO
-    dcc.Markdown('''
-##### To be added:
-                 - Map
-                 - Charts
-                 - Filter
-                 ''')
+    dbc.Row(dbc.Col(title)),
+    dbc.Row([
+        dbc.Col(global_widgets, md=3),
+        dbc.Col(cali_map),
+        dbc.Col([
+            dbc.Row(sum_cost),
+            dbc.Row(damage_level)
+        ],
+        md=3)
+    ]),
+    dbc.Row([
+        dbc.Col(house_damage),
+        dbc.Col(time_cost),
+        dbc.Col(structure_count)
+    ])
 ])
 
 
-# Server side callbacks/reactivity
-@callback(
-    Output('scatter', 'spec'),
-    Input('location', 'value')
-)
-
-def create_chart(location="Butte County"):
-#   brush = alt.selection_point(fields=['Miles_per_Gallon', x_col], name='selection')
-#   Later we'll use brushes for better interactivity with charts
-
-    df = pd.read_csv("../data/raw/California Wildfire Damage.csv", parse_dates=True)
-    df = df[df['Location'] == location]
-    
-    chart = alt.Chart(df).mark_point().encode(
-        x='Injuries',
-        y='Fatalities',
-        tooltip='Incident_ID',
-    ).interactive()
-
-    return chart.to_dict()
 
 # Run the app/dashboard
 if __name__ == '__main__':
