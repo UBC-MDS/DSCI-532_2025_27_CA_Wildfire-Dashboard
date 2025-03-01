4dfb1da65423010b2e7d932d9943eead12773149
Elaine Chu <chuelaine23@gmail.com>
2025-02-28 22:40:37 -0800

    Add src path

--- src/app.py
+++ src/app.py
@@ -5,12 +5,12 @@ import dash_vega_components as dvc
 import pandas as pd
 import geopandas as gpd
 import plotly.express as px
-from data_import import load_calfire_df
-from roof_chart import make_roof_chart
-from damage_chart import make_damage_chart
-from structure_chart import make_structure_chart
-from summary_chart import make_summary_chart
-from timeseries_chart import make_time_series_chart
+from src.data_import import load_calfire_df
+from src.roof_chart import make_roof_chart
+from src.damage_chart import make_damage_chart
+from src.structure_chart import make_structure_chart
+from src.summary_chart import make_summary_chart
+from src.timeseries_chart import make_time_series_chart
 
 
 # Initiatlize the app
