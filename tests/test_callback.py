a61fd087fb5e60cb2db12035d950f5ed306ca016
Elaine Chu <chuelaine23@gmail.com>
2025-03-16 13:25:11 -0700

    Create tests for callback functions

--- tests/test_callback.py
+++ tests/test_callback.py
@@ -1,6 +1,7 @@
 from contextvars import copy_context
 from dash._callback_context import context_value
 from dash._utils import AttributeDict
+from dash import callback_context
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
@@ -18,23 +19,80 @@ def test_toggle_button():
     assert output is False
 
 
-# Test submit button
-def test_update_charts():
+# Test reset button
+def test_reset_button():
 
     def run_callback(trigger):
         context_value.set(AttributeDict(**{"triggered_inputs": [trigger]}))
-        return update_charts(1, 0)
+        return update_charts(0, 1, None, ["Butte"], [2017, 2020], None)
     
     ctx = copy_context()
     trigger={"prop_id": "reset.n_clicks"}
 
     output = ctx.run(run_callback, trigger)
 
-    assert output == (1,0)
+    (roof_chart, damage_chart, structure_chart, summary_card_update, 
+     timeseries_chart, county, year, incident_name) = output
+    
+    assert isinstance(roof_chart, dict), "Returned roof chart should be a dicionary"
+    assert isinstance(damage_chart, dict), "Returned damage chart should be a dictionary"
+    assert isinstance(structure_chart, dict), "Returned structure chart should be a dictionary"
+    assert isinstance(timeseries_chart, dict), "Returned timeseries chart should be a dictionary"
+    assert isinstance(summary_card_update, list), "Returned summary card should be a dictionary"
+    assert county is None, "County input should be cleared"
+    assert year == [2014, 2025], "Year input should reset to min and max years"
+    assert incident_name is None, "Incident name should be cleared"
 
-# Test reset filter 
+# Test year imput
+def test_submit_button():
+
+    def run_callback(trigger):
+        context_value.set(AttributeDict(**{"triggered_inputs": [trigger]}))
+        return update_charts(1, 0, None, [2017, 2020], None, None)
+    
+    ctx = copy_context()
+    trigger={"prop_id": "submit.n_clicks"}
+
+    output = ctx.run(run_callback, trigger)
+
+
+    (roof_chart, damage_chart, structure_chart, summary_card_update, 
+     timeseries_chart, county, year, incident_name) = output
+    print(county),
+    print(incident_name)
+    print(year)
+    assert isinstance(roof_chart, dict), "Returned roof chart should be a dicionary"
+    assert isinstance(damage_chart, dict), "Returned damage chart should be a dictionary"
+    assert isinstance(structure_chart, dict), "Returned structure chart should be a dictionary"
+    assert isinstance(timeseries_chart, dict), "Returned timeseries chart should be a dictionary"
+    assert isinstance(summary_card_update, list), "Returned summary card should be a dictionary"
+    assert county is None
+    assert year == [2017, 2020], "Returned year should be the same as input"
+    assert incident_name is None 
 
 # Test county input
+def test_county_input():
 
-# Test year range input
-# test incident name input
+    def run_callback(trigger):
+        context_value.set(AttributeDict(**{"triggered_inputs": [trigger]}))
+        return update_charts(1, 0, ["Butte"], [2017, 2020], None, None)
+    
+    ctx = copy_context()
+    trigger={"prop_id": "submit.n_clicks"}
+
+    output = ctx.run(run_callback, trigger)
+
+
+    (roof_chart, damage_chart, structure_chart, summary_card_update, 
+     timeseries_chart, county, year, incident_name) = output
+    print(county),
+    print(incident_name)
+    print(year)
+    assert isinstance(roof_chart, dict), "Returned roof chart should be a dicionary"
+    assert isinstance(damage_chart, dict), "Returned damage chart should be a dictionary"
+    assert isinstance(structure_chart, dict), "Returned structure chart should be a dictionary"
+    assert isinstance(timeseries_chart, dict), "Returned timeseries chart should be a dictionary"
+    assert isinstance(summary_card_update, list), "Returned summary card should be a dictionary"
+    assert county == ["Butte"], "Returned County should be same as input"
+    assert year == [2017, 2020], "Returned year should be the same as input"
+    assert incident_name is None, "Returned incident should be none" 
