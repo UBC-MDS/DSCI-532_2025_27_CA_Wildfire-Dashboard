ecb3ba8249b7a7b5d575955fd335a6bf1dc01127
Elaine Chu <chuelaine23@gmail.com>
2025-03-16 18:49:41 -0700

    resolved assertion errors

--- tests/test_summary_chart.py
+++ tests/test_summary_chart.py
@@ -9,13 +9,13 @@ from src.summary_chart import make_summary_chart
 
 # Test cases
 test_cases = [
-    (pd.DataFrame({"Total Economic Loss": [50000000, 200000000, 150000000]}), 0.4, "Normal case with multiple values"),
+    (pd.DataFrame({"Total Economic Loss": [50000000, 200000000, 150000000]}), "$400.00M", "Normal case with multiple values"),
     (pd.DataFrame(columns=["Total Economic Loss"]), 0, "Empty DataFrame"),
     (pd.DataFrame({"Other Column": [100, 200, 300]}), 0, "Missing required column"),
-    (pd.DataFrame({"Total Economic Loss": [1000000000]}), 1.0, "Single value"),
-    (pd.DataFrame({"Total Economic Loss": [1e10, 2e10, 3e10]}), 60.0, "Large values"),
-    (pd.DataFrame({"Total Economic Loss": [-50000000, 200000000, -150000000]}), 0.0, "Negative values"),
-    (pd.DataFrame({"Total Economic Loss": [0, 0, 0]}), 0, "Zero values")
+    (pd.DataFrame({"Total Economic Loss": [1000000000]}), "$1.00B", "Single value"),
+    (pd.DataFrame({"Total Economic Loss": [1e10, 2e10, 3e10]}), "$60.00B", "Large values"),
+    (pd.DataFrame({"Total Economic Loss": [-50000000, 200000000, -150000000]}), "$0.00M", "Negative values"),
+    (pd.DataFrame({"Total Economic Loss": [0, 0, 0]}), "$0.00M", "Zero values")
 ]
 
 @pytest.mark.parametrize("input_df, expected, test_name", test_cases)
